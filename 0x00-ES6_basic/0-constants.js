7a549d215d52dea7202c9690f1d6466794d30263
mgmtsweni <mtswenigift@gmail.com>
2023-01-23 21:02:10 +0200

    Javascript Basics

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+export function taskFirst() {
+    var task = 'I prefer const when i can.';
+    return task
+}
+
+export function getLAst() {
+    return 'is Okay';
+}
+
+export function taskNext() {
+    var combintion = 'But sometimes let';
+    return combintion;
+}
