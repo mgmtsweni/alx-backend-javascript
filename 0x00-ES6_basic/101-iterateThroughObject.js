225b783a270a4d9cb4e3f5bc57f909ba1ecbfb68
mgmtsweni <mtswenigift@gmail.com>
2023-01-26 20:01:05 +0200

    advance lint

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const employees = '';
-    for (const employee of reportWithIterator) {
-        employees += ` ${employee} | `;
-    }
-    return employees.slice(0, -3);
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
 }
