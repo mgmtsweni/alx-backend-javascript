49e7820efdc21e33236587f9266c444e6325defe
mgmtsweni <mtswenigift@gmail.com>
2023-01-30 23:28:45 +0200

    Lint fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(studentListIds) {
-    if (!(studentListIds instanceof Array)) {
-        return []
-    }
-    return studentListIds.map((student) => student.id);
+  if (!(studentListIds instanceof Array)) {
+    return [];
+  }
+  return studentListIds.map((student) => student.id);
 }
